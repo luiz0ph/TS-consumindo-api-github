020cd12c9e1867a32a5763b8d8773f25a0e8f5cf
Luiz Phelipe <luizphsantana@gmail.com>
2024-09-23 21:11:47 -0300

    começo do desenvolvimento da leitura dos repositorios

--- public/dist/index.js
+++ public/dist/index.js
@@ -22,12 +22,39 @@ function requisicaoGithub(nomeDoUser) {
             const novoUser = new User(dado.id, dado.login, dado.name, dado.bio, dado.public_repos, dado.repos_url);
             users.push(novoUser);
             console.log(`O usuario ${novoUser.name} foi adicionado na lista`);
+            infoUser('luiz0ph');
         });
     }))
         .catch((err) => console.error(`Erro: ${err}`));
 }
 // Teste com meu github
 requisicaoGithub('luiz0ph');
+// Função para procurar um usuario em especifico
+function procurarUser(login) {
+    // No TS o find parece dar erro (NÃO DA ERRO NO JS)
+    return users.find(user => user.login === login);
+}
+function infoUser(login) {
+    const user = procurarUser(login);
+    if (!user) {
+        return "Usuario não encontrado";
+    }
+    console.log(`Usuario: ${user.login}`);
+    fetch(user.repos_url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => res.json().then(dado => {
+        // Exibir os dados dos 3 primeiros repositorios
+        console.log(`Repositorio: ${dado[0].name}`); // Nome do repositorio
+        console.log(`Descrição: ${dado[0].description}`); // Descrição
+        console.log(`Fork: ${dado[0].fork}`); // Fork
+        console.log(`stargazers_count: ${dado[0].stargazers_count}`);
+    }))
+        .catch((err) => console.error(`Erro: ${err}`));
+}
 // Listar usuarios salvos na lista
 function listarUsuarios() {
     console.log(users);
--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,7 @@ function requisicaoGithub(nomeDoUser: string) {
                 const novoUser = new User(dado.id, dado.login, dado.name, dado.bio, dado.public_repos, dado.repos_url);
                 users.push(novoUser);
                 console.log(`O usuario ${novoUser.name} foi adicionado na lista`);
+                infoUser('luiz0ph');
             });
         }))
         .catch((err) => console.error(`Erro: ${err}`))
@@ -46,6 +47,36 @@ function requisicaoGithub(nomeDoUser: string) {
 
 // Teste com meu github
 requisicaoGithub('luiz0ph');
+
+// Função para procurar um usuario em especifico
+function procurarUser(login: string) {
+    // No TS o find parece dar erro (NÃO DA ERRO NO JS)
+    return users.find(user => user.login === login);
+}
+
+function infoUser(login: string) {
+    const user = procurarUser(login);
+    if (!user) {
+        return "Usuario não encontrado";
+    }
+
+    console.log(`Usuario: ${user.login}`);
+    fetch(user.repos_url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }})
+            .then(res => res.json().then(dado => {
+                // Exibir os dados dos 3 primeiros repositorios
+                console.log(`Repositorio: ${dado[0].name}`) // Nome do repositorio
+                console.log(`Descrição: ${dado[0].description}`) // Descrição
+                console.log(`Fork: ${dado[0].fork}`) // Fork
+                console.log(`Estrelas: ${dado[0].stargazers_count}`) // Estrelas
+
+                // Terminar mais tarde de fazer os outros dois
+            }))
+            .catch((err) => console.error(`Erro: ${err}`));
+}
 
 // Listar usuarios salvos na lista
 function listarUsuarios() {
